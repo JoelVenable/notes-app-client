6b628c74acb448ab750787e4a77869c09b9608c7
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-28 22:22:56 -0500

    Added home component and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import { MyNavbar } from "./components/navbar";
+import { MyNavbar } from "./components/MyNavbar";
+import { Home } from "./components/Home";
 
 function App() {
   return (
     <div className="App container">
       <MyNavbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './home.css';
+
+export const Home = () => (
+  <div className="Home">
+    <div className="Lander">
+      <h1>Scratch</h1>
+      <p>A simple note taking app</p>
+    </div>
+  </div>
+);
