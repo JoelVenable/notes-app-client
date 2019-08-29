5a373a9d7f44fa7c38c9aa2f536d649121cd633e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-28 23:11:15 -0500

    Added not found route and component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "./components/Home";
+import { NotFound } from "./components/NotFound";
 
 export const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route component={NotFound} />
   </Switch>
 );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const NotFound = () => (
+  <div style={{ paddingTop: "100px", textAlign: "center" }}>
+    <h3>Sorry, there's nothing here...</h3>
+  </div>
+);
