5c5e5645ddff23272672baa54f88aecd99c8732f
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-29 23:01:24 -0500

    Loaded login state from Amplify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
 import { MyNavbar } from "./components/MyNavbar";
 import { Routes } from "./Routes";
+import { AuthContext } from "./AuthContext";
 
 function App() {
-  return (
+  const { authResolving } = useContext(AuthContext);
+  return authResolving ? null : (
     <div className="App container">
       <MyNavbar />
       <Routes />
--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,12 +1,33 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setAuthentication] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authResolving, setAuthResolving] = useState(true);
+
+  const getAuth = async () => {
+    try {
+      await Auth.currentSession();
+      setIsAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+    setAuthResolving(false);
+  };
+
+  useEffect(() => getAuth(), []);
+
+  const setAuthentication = () => {
+    setIsAuthenticated(false);
+  };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setAuthentication }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, setAuthentication, authResolving }}>
       {children}
     </AuthContext.Provider>
   );
