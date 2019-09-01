1c42143916acbbc66359074674ca9578074e51a0
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-01 16:47:50 -0500

    Added "Add a note" button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { LinkContainer } from 'react-router-bootstrap';
 import './home.css';
+import { LoaderButton } from "./LoaderButton";
 
 export const Home = () => (
   <div className="Home">
     <div className="Lander">
       <h1>Scratch</h1>
       <p>A simple note taking app</p>
+      <LinkContainer to="/notes/add">
+        <LoaderButton block bsSize="lg" text="Add a Note" />
+      </LinkContainer>
     </div>
   </div>
 );
